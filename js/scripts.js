30a68446dcddf6510f40ac8c0e8e08bfd685db3b
Guest User <Guest@Epicodus-10E.local>
2017-11-29 20:52:51 -0800

    add initial html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,43 @@
 // back end logic
-function Order(name, address, size, toppings) {
-	this.name = name;
-	this.address = address;
+function Pie (size, toppings) {
 	this.size = size;
-	this.toppings = [];
+	this.toppings = toppings;
+}
+
+var pizzaSize = [];
+var pizzaToppings = [];
+
+Pie.prototype.price = function() {
+	var price = 0;
+	if (this.size === "small") {
+		price += 5;
+	} else if (this.size === "medium") {
+		price += 6;
+	} else if (this.size === "large") {
+		price += 7;
+	} else {
+		price += 8;
+	} if (pizzaToppings === 1) {
+		price += 1.5;
+	} else if (pizzaToppings === 2) {
+		price += 3;
+	} else if (pizzaToppings === 3) {
+		price += 4.5;
+	} else {
+		price += 6;
+	} return price;
 };
 
-// front end logic
+//UI logic
 
 $(document).ready(function() {
-	$("form#order").submit(function(event) {
-		event.preventDefault();
-		var inputtedName = $("#name").val();
-		var inputtedAddress = $("#address").val();
-		var inputtedSize = $("#size").val();
-		var inputtedToppings = $("#toppings").val();
-		newOrder = new Order(inputtedName, inputtedAddress, inputtedSize, inputtedToppings);
-		console.log(newOrder);
-
+  $("form#tickets").submit(function(event) {
+    event.preventDefault();
+		var inputtedSize = $("#pizza-size").val();
+	$("input:checkbox[name=toppings]:checked").each(function() {
+		var inputtedToppings = $(this).val();
+		toppings.push(inputtedToppings);
+	})
+	$("#total").text(price);
 	});
-
 });
