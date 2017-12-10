76b26d62ca954ae60115372f252e3f8c1ae82527
MJ <mdjacksonvideo@gmail.com>
2017-12-09 22:42:05 -0800

    make submit button reset price

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,39 @@ function Pie (size, toppings) {
 }
 
 var pizzaSize = [];
+
 var pizzaToppings = [];
-var sizePrice = 0;
-var toppingsPrice = 0;
+
+var price = 0;
 Pie.prototype.sizePrice = function() {
 	if (this.size === "Small") {
-		sizePrice += 5;
+		price += 5;
 	} else if (this.size === "Medium") {
-		sizePrice += 6;
+		price += 6;
 	} else if (this.size === "Large") {
-		sizePrice += 7;
+		price += 7;
 	} else {
-		sizePrice += 8;
+		price += 8;
 	}
-	return sizePrice;
+	return price;
 };
 Pie.prototype.toppingsPrice = function() {
-	if (pizzaToppings.length === 1) {
-	toppingsPrice += 1.5;
+	if (pizzaToppings.length === 0) {
+	price += 0;
+	} else if (pizzaToppings.length === 1) {
+	price += 1.5;
 	} else if (pizzaToppings.length === 2) {
-	toppingsPrice += 3;
+	price += 3;
 	} else if (pizzaToppings.length === 3) {
-	toppingsPrice += 4.5;
+	price += 4.5;
 	} else {
-	toppingsPrice += 6;
+	price += 6;
 	}
-	return toppingsPrice;
+	return price;
+};
+
+Pie.prototype.reset = function() {
+	price=0;
 };
 
 //UI logic
@@ -39,13 +46,17 @@ $(document).ready(function() {
 	$("form#pizza-order").submit(function(event){
     event.preventDefault();
    $("input:checkbox[name=toppings-order]:checked").each(function(){
-       var InputtedToppings = $(this).val();
-       pizzaToppings.push(InputtedToppings);
-			 var inputtedSize = $('#size option:selected').val();
-			 pizzaSize.push(inputtedSize);
-			 console.log(pizzaToppings)
-			 console.log(pizzaToppings)
-		});
+     var InputtedToppings = $(this).val();
+     pizzaToppings.push(InputtedToppings);
+	 });
+		var inputtedSize = $('#size option:selected').val();
+		var userPie = new Pie (inputtedSize, pizzaToppings);
+		pizzaSize.push(inputtedSize);
+		userPie.sizePrice();
+		userPie.toppingsPrice();
+		$("#total").text("$" + price);
+		userPie.reset();
+
 	});
 });
 // 		var inputtedSize = $('#size option:selected').val();
